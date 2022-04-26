2b1be7e0ea3dc482373ff6800e77920578faeb08
nailgun <hernybiotec@gmail.com>
2022-04-26 09:40:30 +0100

    .

--- brain_from_side.py
+++ brain_from_side.py
@@ -3,6 +3,7 @@
 """
 from brainrender import Scene
 from brainrender import settings
+from brainrender.actors import Cylinder
 
 settings.SHOW_AXES = False
 settings.WHOLE_SCREEN = False
@@ -25,10 +26,47 @@ print(f"[{orange}]Running\: {Path(__file__).name}")
 
 # Plot all together:
 scene = Scene(inset=False, title="")
-region = scene.add_brain_region("CP", alpha=0.2)
+region = scene.add_brain_region("CP", alpha=0.2, color="green")
 scene.content
-scene.render(camera=top_camera, zoom=1.2, interactive=True)
-plane = scene.atlas.get_plane(pos=plane_center, norm=(-1, 0, 0))
-scene.slice(plane)
+
+# Slice actors with frontal plane
+# scene.slice("sagital", actors=[th])
+
+# Slice with a custom plane
+# plane = scene.atlas.get_plane(pos=mos.centerOfMass(), norm=(1, 1, 0))
+scene.slice('sagittal')#, actors=[region])
+
+
+# plane = scene.atlas.get_plane(pos=plane_center, norm=(0, 0, -1))
+# scene.slice(plane)
+
+print(region.mesh.bounds()[0])
+print(region.points().mean(axis=0))
+
+# create and add a cylinder actor
+cil_tail = Cylinder(
+    pos=[7300, 3900, 2100],  # center the cylinder at the center of mass of th
+    root=scene.root,  # the cylinder actor needs information about the root mesh
+    color="red",
+    alpha=1,
+    radius=300
+)
+
+scene.add(cil_tail)
+
+
+cil_nac = Cylinder(
+    pos=[4700, 5200, 4000],  # center the cylinder at the center of mass of th
+    root=scene.root,  # the cylinder actor needs information about the root mesh
+    color="red",
+    alpha=1,
+    radius=300
+)
+
+scene.add(cil_nac)
+
+
+scene.render(camera='sagittal', zoom=1.2, interactive=True)
+
 # scene.screenshot(name='back_view_all.png')
 scene.close()
